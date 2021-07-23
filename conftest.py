97adfe53acb0a0d8aa30fd117eb4237fa0889b3e
Vadim S <svershchevskiy.vadim@gmail.com>
2021-07-24 01:47:06 +0300

    added simple test

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Usage exaple: pytest --language=es test_items.py")
+
+
+@pytest.fixture()
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
